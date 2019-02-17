da94cfd7a765157558f14f29e026c30ef1ed9384
CalvQ <calvin.qin.ca@gmail.com>
2019-02-16 22:41:23 -0800

    Rando

--- AHRS_10DOF.py
+++ AHRS_10DOF.py
@@ -1,13 +1,22 @@
-import adafruit_l3gd20
-import adafruit_lsm303
+import importlib.util
+specG = importlib.util.spec_from_file_location("adafruit_l3gd20", "../Adafruit_CircuitPython_Bundle/libraries/drivers/l3gd20/adafruit_l3gd20.py")
+specA = importlib.util.spec_from_file_location("adafruit_lsm303", "../Adafruit_CircuitPython_Bundle/libraries/drivers/lsm303/adafruit_lsm303.py")
+#import adafruit-circuitpython-l3gd20 as l3gd
+#import adafruit-circuitpython-lsm303 as lsm
+
+l3gd = importlib.util.module_from_spec(specG)
+lsm = importlib.util.module_from_spec(specA)
+
+specG.loader.exec_module(l3gd)
+specA.loader.exec_module(lsm)
 
 import busio
 import board
 
 i2c = busio.I2C(board.SCL, board.SDA)
 
-gyro = adafruit_l3gd20.L3GD20_I2C(i2c)
-accel = adafruit_lsm303.LSM303(i2c)
+gyro = l3gd.L3GD20_I2C(i2c)
+accel = lsm.LSM303(i2c)
 
 timeV = time.time()
 
---
+++ test.py
@@ -0,0 +1,5 @@
+imu = "/dev/i2c-1"
+fo = open(l3gd20, "rb")
+print fo.read()
+address = 0x6B
+
