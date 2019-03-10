2794141b81184f298f5a297afd5c789e51db8a35
QinCalvin <calvin.qin.ca@gmail.com>
2019-03-09 16:26:12 -0800

    New sensor file

---
+++ FlightController/l3gd20.py
@@ -0,0 +1,11 @@
+import adafruit_l3gd20 as lgd
+
+from busio import I2C
+from board import SDA, SCL
+
+i2c = I2C(SCL, SDA)
+
+sensor = adafruit_l3gd20.L3GD20_I2C(i2c)
+
+while(True):
+    print(sensor.gyro)
